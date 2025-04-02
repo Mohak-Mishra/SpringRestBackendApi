c3ab6dbce115e64e49df7f2a984dad1331c6a206
Mohak Mishra <mohakmishra2207@gmail.com>
2025-04-02 12:51:57 +0530

    second

--- src/main/java/com/mishra/mohak/springrest02/bean/Student.java
+++ src/main/java/com/mishra/mohak/springrest02/bean/Student.java
@@ -3,6 +3,8 @@ package com.mishra.mohak.springrest02.bean;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +21,8 @@ public class Student {
 
     private Integer sid;
     @NotBlank(message = "name can't be blank")
+    @Size(min = 3,max=25 ,message = "length must be in between 3-25 ")
+    @Pattern(regexp = "[a-zA-Z]{2,20}")
     private String sname;
     @NotBlank(message = "dob can't be blank"
     )
@@ -26,6 +30,7 @@ public class Student {
     @NotBlank(message = "gender can't be blank")
     private String sgender;
     @NotBlank(message = "address can't be blank")
+    @Pattern(regexp = "[a-zA-z #-@\\.\\-1-9]{2,20}")
     private String saddress;
     private String scourse;
 
--- src/main/java/com/mishra/mohak/springrest02/restController/StudentRestController.java
+++ src/main/java/com/mishra/mohak/springrest02/restController/StudentRestController.java
@@ -2,10 +2,18 @@ package com.mishra.mohak.springrest02.restController;
 
 import com.mishra.mohak.springrest02.bean.Student;
 import com.mishra.mohak.springrest02.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.persistence.Id;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,31 +24,111 @@ public class StudentRestController {
     @Autowired
     private StudentService studentService;
 
-
+    @Operation(summary = "Save the Student Object")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201", description = "CREATED A RESOURCE AT PROVIDER",
+                            content = {
+                                    @Content(mediaType = "text/plain")
+                            }
+                    ),
+                    @ApiResponse(responseCode = "400", description = "BAD REQUEST",
+                            content =
+                                    {
+                                            @Content(mediaType ="application/json")
+                                    }
+                    )
+            }
+    )
     @PostMapping("/save")
-    public ResponseEntity<String> saveStudent(@RequestBody Student student) {
+    public ResponseEntity<String> saveStudent(@RequestBody @Valid Student student) {
        int i= studentService.saveStudent(student);
         return ResponseEntity.ok("Student got saved with id " + i);
     }
-
+    @Operation(summary = "Delete a Student by its id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200", description = "SUCCESS",
+                            content = {
+                                    @Content(mediaType = "plain/text")
+                            }
+                    ),
+                    @ApiResponse(responseCode = "404", description = "Student not found",
+                            content =
+                                    {
+                                            @Content(mediaType ="application/json", schema = @Schema(implementation =ErrorResponse.class))
+                                    }
+                    )
+            }
+    )
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteStudent(@PathVariable Integer id) {
         studentService.deleteStudent(id);
         return ResponseEntity.ok("Deleted with id " + id);
     }
-
+    @Operation(summary = "Update Student object")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200", description = "SUCCESS",
+                            content = {
+                                    @Content(mediaType = "plain/text")
+                            }
+                    ),
+                    @ApiResponse(responseCode = "404", description = "Student not found",
+                            content =
+                                    {
+                                            @Content(mediaType ="application/json", schema = @Schema(implementation =ErrorResponse.class))
+                                    }
+                    )
+            }
+    )
     @PutMapping("/update")
-    public ResponseEntity<String> updateStudent(@RequestBody Student student) {
+    public ResponseEntity<String> updateStudent(@RequestBody @Valid Student student) {
         studentService.updateStudent(student);
         return new ResponseEntity<>("Student got updated with id"+ student.getSid(), HttpStatus.OK);
     }
-
+    @Operation(summary = "Find a Student by its id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200", description = "SUCCESS",
+                            content = {
+                                    @Content(mediaType = "application/json" , schema = @Schema(implementation =Student.class))
+                            }
+                    ),
+                    @ApiResponse(responseCode = "404", description = "Student not found",
+                            content =
+                                    {
+                                            @Content(mediaType ="application/json", schema = @Schema(implementation = ErrorResponse.class))
+                                    }
+                    )
+            }
+    )
     @GetMapping("find/{id}")
     public ResponseEntity<Student> findStudent(@PathVariable Integer id) {
         return new ResponseEntity<>(studentService.getOneStudent(id), HttpStatus.OK);
     }
+    @Operation(summary = "Find all Student")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200", description = "SUCCESS",
+                            content = {
+                                    @Content(mediaType = "application/json" ,
+                                            array =
+                                            @ArraySchema(
+                                                    schema = @Schema(implementation = Student.class)
+                                            )
+                                    )
 
+                            }
+                    )
+            }
+    )
     @GetMapping("findAllStudents")
     public ResponseEntity<List<Student>> findAllStudents() {
         return new ResponseEntity<>(studentService.getAllStudents(), HttpStatus.OK);
